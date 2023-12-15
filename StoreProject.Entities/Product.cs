60d6a4fb2d4a7e7f86e4564300bc03c2426989f8
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-12-15 18:52:20 +0300

    Add project files.

---
+++ StoreProject.Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreProject.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
