a38080ab534b38761451cdc33008b558b6a1ddc8
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-12-15 18:54:37 +0300

    entitymodels

--- StoreProject.Entities/Models/Product.cs
+++ StoreProject.Entities/Models/Product.cs
@@ -1,4 +1,10 @@
-﻿namespace StoreProject.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProject.Entities.Models
 {
     public class Product
     {
