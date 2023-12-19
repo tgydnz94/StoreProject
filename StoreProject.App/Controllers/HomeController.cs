93fefcecf3aa41bd3e8bec667bd1fd4307fc2ae6
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-12-19 19:24:15 +0300

    mvc

---
+++ StoreProject.App/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreProject.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
