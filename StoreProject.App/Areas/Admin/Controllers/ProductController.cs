39f5a58c11bb772501f0a63cbcfc1919cdab5e37
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-12-20 21:09:34 +0300

    admin layout

---
+++ StoreProject.App/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreProject.App.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
